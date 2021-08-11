53e6d1586f9f6a7e171548e2bf64b379df577690
Artem Fedoseev <afedoseev359@gmail.com>
2021-08-11 12:09:22 +0300

    Updates

---
+++ precache-manifest.ad63d4024274da24c89321473a9bdf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d6337adbf9282001b19c73b9bd3585",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f78864a2ff990212b902",
+    "url": "./static/css/main.71f3fcb3.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78864a2ff990212b902",
+    "url": "./static/js/main.e3b8448e.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
